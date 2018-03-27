a3a3286f855c2a762503b95cdede811d3af7bd1f
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-27 21:23:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,9 +7,9 @@ class Artist
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
@@ -19,7 +19,7 @@ class Artist
   def self.all
     @@artists
   end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
